1ca7c7495f70fc5cd4c12214347a9caf6f9bb30d
panhalkarpravin <pspanhalkar@gmail.com>
2023-09-15 19:09:35 +0530

    updated

--- src/component/RouteConfig.tsx
+++ src/component/RouteConfig.tsx
@@ -1,15 +1,15 @@
 import { Routes, Route } from "react-router-dom";
 import NoFound from "../pages/Notfound/NoFound";
 import { Login } from "../pages/Login/Login";
-import { Dashboard } from "@mui/icons-material";
 import EmpList from "../pages/EmployeeList/EmpList";
+import { Dashboard } from "../pages/Dashboard/Dashboard";
 
 export const RouteConfig = () => {
   return (
     <>
       <Routes>
-        <Route path="/Dashboard" element={<Dashboard />} />
-        <Route path="//list" element={<EmpList />} />
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/list" element={<EmpList />} />
         <Route path="/login" element={<Login />} />
         <Route path="*" element={<NoFound />} />
       </Routes>
